09a79f235f9f1ab6f732fcab32147d67e67435fd
Audrey Norris <runanord13@gmail.com>
2026-02-18 15:34:48 -0800

    Added Base Project

---
+++ 310Bluetooth/310Bluetooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  310Bluetooth
+//
+//  Created by Audrey Norris on 2/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 310Bluetooth/310Bluetooth/_10BluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  _10BluetoothApp.swift
+//  310Bluetooth
+//
+//  Created by Audrey Norris on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct _10BluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
