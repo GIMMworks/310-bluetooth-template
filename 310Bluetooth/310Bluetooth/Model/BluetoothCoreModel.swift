b8ad67b80e0dd9c2c1b5b964887c2d60afe405aa
Audrey Norris <runanord13@gmail.com>
2026-02-18 16:55:52 -0800

    Pushing Basic Bluetooth System Setup

---
+++ 310Bluetooth/310Bluetooth/Model/BluetoothCoreModel.swift
@@ -0,0 +1,8 @@
+//
+//  BluetoothCoreModel.swift
+//  310Bluetooth
+//
+//  Created by Audrey Norris on 2/18/26.
+//
+
+import Foundation
---
+++ 310Bluetooth/310Bluetooth/Views/BluetoothCoreView.swift
@@ -0,0 +1,18 @@
+//
+//  BluetoothCoreView.swift
+//  310Bluetooth
+//
+//  Created by Audrey Norris on 2/18/26.
+//
+
+import SwiftUI
+
+struct BluetoothCoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BluetoothCoreView()
+}
