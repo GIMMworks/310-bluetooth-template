b36739ac557312a53f77cb558535d071e98da115
Audrey Norris <runanord13@gmail.com>
2026-03-11 15:04:55 -0700

    Added IBeacon Setup

---
+++ 310Bluetooth/310Bluetooth/Model/IBeaconModel.swift
@@ -0,0 +1,8 @@
+//
+//  IBeaconModel.swift
+//  310Bluetooth
+//
+//  Created by Audrey Norris on 3/11/26.
+//
+
+import Foundation
---
+++ 310Bluetooth/310Bluetooth/Views/IBeaconView.swift
@@ -0,0 +1,18 @@
+//
+//  IBeaconView.swift
+//  310Bluetooth
+//
+//  Created by Audrey Norris on 3/11/26.
+//
+
+import SwiftUI
+
+struct IBeaconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IBeaconView()
+}
